cb2436c9ef3dcf4060adda74d72918c25ee4ac39
se2736794 <139154268+se2736794@users.noreply.github.com>
2023-08-19 21:30:54 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelector('.button').addEventListener('click' , function(){
+    a = document.querySelector('.ekran')
+    a.value = parseInt(a.value) +1
+    console.log(a.value);
+})
+
+document.querySelector('.dalete').addEventListener('click' , function(){
+    a = document.querySelector('.ekran')
+    ochirish = 0
+    a.value = ochirish
+    console.log(ochirish);
+
+})
+
